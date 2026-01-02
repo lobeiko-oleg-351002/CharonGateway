9cdc372d7527d9e6d05c8a14773218f04d670753
totalit <totalit2803@gmail.com>
2026-01-02 16:22:30 +0100

    Add Daily Average Metrics endpoint and adjust json processing in service

--- src/CharonGateway/Models/DTOs/DailyAverageMetricDto.cs
+++ src/CharonGateway/Models/DTOs/DailyAverageMetricDto.cs
@@ -1,13 +1,25 @@
+using System.Text.Json.Serialization;
+
 namespace CharonGateway.Models.DTOs;
 
 public class DailyAverageMetricDto
 {
+    [JsonPropertyName("date")]
     public DateTime Date { get; set; }
+    
+    [JsonPropertyName("type")]
     public string Type { get; set; } = string.Empty;
+    
+    [JsonPropertyName("name")]
     public string Name { get; set; } = string.Empty;
+    
+    [JsonPropertyName("averageValues")]
     public Dictionary<string, double> AverageValues { get; set; } = new();
+    
+    [JsonPropertyName("count")]
     public int Count { get; set; }
 }
+
 
 
 
